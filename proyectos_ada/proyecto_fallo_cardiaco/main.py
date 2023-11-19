470e5cbdf0336255efa340c2120031fdfd1001df
Jhefrey <jhefreyzerdna@gmail.com>
2023-11-19 17:42:25 -0500

    Part 2 done

--- proyectos_ada/proyecto_fallo_cardiaco/main.py
+++ proyectos_ada/proyecto_fallo_cardiaco/main.py
@@ -1,10 +1,25 @@
 from datasets import load_dataset
+import numpy as np
+
+# PARTE 1
+
+dataset = load_dataset("mstz/heart_failure")
+
+data = dataset["train"]
+
+edades = np.array(data["age"])
+
+media_edad = np.mean(edades)
+print("El promedio de la edad de los participantes de este estudio es :", media_edad, "años")
+
+# PARTE 2
+
 import pandas as pd
 
 # se accede al dataset
-dataset = load_dataset("mstz/heart_failure")
+  # dataset = load_dataset("mstz/heart_failure") YA SE HIZO ARRIBA
 # se accede a train y se guada como data
-data = dataset["train"]
+  # data = dataset["train"] YA SE HIZO ARRIBA IGUAL
 # se convierte la info obtenida en un dataframe
 df = pd.DataFrame(data)
 
@@ -17,3 +32,4 @@ media_df1 = df1["age"].mean()
 print(media_df1)
 media_df2 = df2["age"].mean()
 print(media_df2)
+
