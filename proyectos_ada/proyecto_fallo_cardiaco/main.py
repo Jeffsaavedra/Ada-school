0bcec7d604f7ce7427b0d34878fd93b4d3ab434c
Jhefrey <jhefreyzerdna@gmail.com>
2024-01-27 14:13:28 -0500

    part 12 done

--- proyectos_ada/proyecto_fallo_cardiaco/main.py
+++ proyectos_ada/proyecto_fallo_cardiaco/main.py
@@ -139,12 +139,25 @@ limpieza_preparacion_datos(df)
 # ------------>>>>>>>>>>>> PARTE 11 <<<<<<<<<<<<------------
 
 import pandas as pd
-from clasificacion import graficar_distribucion_clases, particion_estratificada, ajustar_arbol_decision
+from clasificacion import graficar_distribucion_clases_1, particion_estratificada_1, ajustar_arbol_decision_1
 
 # Cargar el DataFrame con los datos procesados
 df = pd.read_csv('datos_procesados.csv')
 
 # Utilizar las funciones de clasificación
-graficar_distribucion_clases(df)
-X_train, X_test, y_train, y_test = particion_estratificada(df)
-ajustar_arbol_decision(X_train, X_test, y_train, y_test)
+graficar_distribucion_clases_1(df)
+X_train, X_test, y_train, y_test = particion_estratificada_1(df)
+ajustar_arbol_decision_1(X_train, X_test, y_train, y_test)
+
+# ------------>>>>>>>>>>>> PARTE 12 <<<<<<<<<<<<------------
+
+import pandas as pd
+from clasificacion import graficar_distribucion_clases_2, particion_estratificada_2, ajustar_arbol_decision_2
+
+# Cargar el DataFrame con los datos procesados
+df = pd.read_csv('datos_procesados.csv')
+
+# Utilizar las funciones de clasificación
+graficar_distribucion_clases_2(df)
+X_train, X_test, y_train, y_test = particion_estratificada_2(df)
+ajustar_arbol_decision_2(X_train, X_test, y_train, y_test)
