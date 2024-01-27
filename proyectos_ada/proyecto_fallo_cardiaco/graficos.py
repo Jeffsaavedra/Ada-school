4b50cfa466f6dd5d6434f998b4b5fdbf25b3c9c3
Jhefrey <jhefreyzerdna@gmail.com>
2024-01-27 12:27:01 -0500

    part 8 done

--- proyectos_ada/proyecto_fallo_cardiaco/graficos.py
+++ proyectos_ada/proyecto_fallo_cardiaco/graficos.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import seaborn as sns
 
 # ------------>>>>>>>>>>>> PARTE 7  <<<<<<<<<<<<------------
 
@@ -61,4 +62,34 @@ ax.set_xlabel('Categorias')
 fig.legend(['Hombres', 'Mujeres'], loc='upper right', fontsize='small')
 
 fig.savefig('comparacion_por_genero_.png')
+plt.show()
+
+# ------------>>>>>>>>>>>> PARTE 8  <<<<<<<<<<<<------------
+
+
+sns.set(style="whitegrid")
+fig, axs = plt.subplots(2, 2, figsize=(10, 8))
+
+def plot_pie(ax, data, labels_map, title):
+    values = data.value_counts()
+    labels = [labels_map[x] for x in values.index]
+    ax.pie(values, labels=labels, autopct='%1.1f%%', startangle=90, colors=sns.color_palette('pastel'))
+    ax.set_title(title)
+
+labels_map_anemicos = {0: 'No', 1: 'Si'}
+plot_pie(axs[0, 0], df['anaemia'], labels_map_anemicos, 'Anemicos')
+
+labels_map_diabetes = {0: 'No', 1: 'Si'}
+plot_pie(axs[0, 1], df['diabetes'], labels_map_diabetes, 'Diabeticos')
+
+labels_map_fumador = {0: 'No', 1: 'Si'}
+plot_pie(axs[1, 0], df['smoking'], labels_map_fumador, 'Fumadores')
+
+labels_map_muertos = {0: 'No', 1: 'Si'}
+plot_pie(axs[1, 1], df['DEATH_EVENT'], labels_map_muertos, 'Fallecidos')
+
+plt.subplots_adjust(wspace=0.4, hspace=0.4)
+fig.suptitle('Diagramas de Torta por Característica', fontsize=16)
+
+fig.savefig('graficos_tortas.png')
 plt.show()
--- proyectos_ada/proyecto_fallo_cardiaco/readme.md
+++ proyectos_ada/proyecto_fallo_cardiaco/readme.md
@@ -3,4 +3,4 @@ Bienvenido al proyecto de procesamiento de datos
 
 * las partes 1 a la 5 se encuentran desglozadas en el archivo "main.py"
 * La parte 6 del proyecto está en el archivo "automatización.py"
-* El código de la parte 7 está contenida en "graficos.py"
+* El código de las partes 7 y 8 están contenidas en "graficos.py"
