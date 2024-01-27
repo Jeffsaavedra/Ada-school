188713b5b5f7e127ebe2525b57892547a156bad1
Jhefrey <jhefreyzerdna@gmail.com>
2024-01-27 12:36:44 -0500

    part 9 done

--- proyectos_ada/proyecto_fallo_cardiaco/graficos.py
+++ proyectos_ada/proyecto_fallo_cardiaco/graficos.py
@@ -2,6 +2,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import seaborn as sns
+import plotly.graph_objects as go
+import pandas as pd
+from sklearn.manifold import TSNE
 
 # ------------>>>>>>>>>>>> PARTE 7  <<<<<<<<<<<<------------
 
@@ -66,7 +69,6 @@ plt.show()
 
 # ------------>>>>>>>>>>>> PARTE 8  <<<<<<<<<<<<------------
 
-
 sns.set(style="whitegrid")
 fig, axs = plt.subplots(2, 2, figsize=(10, 8))
 
@@ -93,3 +95,46 @@ fig.suptitle('Diagramas de Torta por Característica', fontsize=16)
 
 fig.savefig('graficos_tortas.png')
 plt.show()
+
+# ------------>>>>>>>>>>>> PARTE 9  <<<<<<<<<<<<------------
+
+# Paso 1: Eliminar la columna objetivo y convertir a array
+X = df.drop(columns=['DEATH_EVENT', 'age']).values
+
+# Paso 2: Exportar un array unidimensional de la columna objetivo
+y = df['DEATH_EVENT'].values
+
+# Paso 3: Ejecutar el algoritmo t-SNE
+X_embedded = TSNE(
+    n_components=3,
+    learning_rate='auto',
+    init='random',
+    perplexity=3
+).fit_transform(X)
+
+# Paso 4: Crear un gráfico de dispersión 3D con Plotly
+fig = go.Figure()
+
+for label in set(y):
+    indices = y == label
+    scatter = go.Scatter3d(
+        x=X_embedded[indices, 0],
+        y=X_embedded[indices, 1],
+        z=X_embedded[indices, 2],
+        mode='markers',
+        marker=dict(size=8, opacity=0.6),
+        name=f'Clase {label}'
+    )
+    fig.add_trace(scatter)
+
+# Configuraciones adicionales del diseño
+fig.update_layout(scene=dict(
+                    xaxis_title='Dimensión 1',
+                    yaxis_title='Dimensión 2',
+                    zaxis_title='Dimensión 3'),
+                    width=800, height=800,
+                    margin=dict(l=0, r=0, b=0, t=0))
+
+# Guardar o mostrar la visualización
+fig.write_html('scatter_3d_plotly.html')  # Puedes guardar el gráfico en un archivo HTML
+# fig.show()  # O mostrarlo directamente en el entorno de ejecución
